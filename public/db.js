e6d8d0ef95a82720e9cb0930471e9ab2d50315aa
ludie lambright <ludiefletcher@aol.com>
2022-02-03 09:50:05 -0800

    added checkDatabase function in db.js

--- public/db.js
+++ public/db.js
@@ -17,3 +17,41 @@ request.onupgradeneeded = function (e) {
     db.createObjectStore("BudgetStore", { autoIncrement: true });
   }
 };
+
+request.onerror = function (e) {
+  console.log(`OH NO! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+  console.log("check db inititalized");
+
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  const store = transaction.objectStore("BudgetStore");
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            currentStore.clear();
+            console.log("Clearing store!");
+          }
+        });
+    }
+  };
+}
