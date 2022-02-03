3fc60e68435d0a273b8b9b1411ed7bef89caa440
ludie lambright <ludiefletcher@aol.com>
2022-02-03 09:42:20 -0800

    added on upgrade function to db.js

--- public/db.js
+++ public/db.js
@@ -2,3 +2,18 @@ let db;
 let budgetVersion;
 
 const request = indexedDB.open("BudgerDB", budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log("There is an upgrade needed in IndexDB");
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.newVersion;
+
+  console.log(`DB updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
+  }
+};
